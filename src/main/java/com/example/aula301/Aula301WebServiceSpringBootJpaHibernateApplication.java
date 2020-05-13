478404f335355d0edf71b01b9f16e2f79dc0efaf
lexpierini <alex_pierini@hotmail.com>
2020-05-13 10:05:25 -0400

    Projet crée

---
+++ src/main/java/com/example/aula301/Aula301WebServiceSpringBootJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.aula301;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula301WebServiceSpringBootJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula301WebServiceSpringBootJpaHibernateApplication.class, args);
+	}
+
+}
