0407947e6adda790ea1965a6ab0cd2cab82ba902
lexpierini <alex_pierini@hotmail.com>
2020-05-13 13:05:16 -0400

    Couche de service, enregistrement des composants

--- src/main/java/com/example/aula301/resources/UtilisateurResource.java
+++ src/main/java/com/example/aula301/resources/UtilisateurResource.java
@@ -1,20 +1,34 @@
 package com.example.aula301.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.aula301.entities.Utilisateur;
+import com.example.aula301.services.UtilisateurService;
 
 @RestController
 @RequestMapping(value = "/utilisateurs")
 public class UtilisateurResource {
 	
+	@Autowired
+	private UtilisateurService service;
+	
 	@GetMapping
-	public ResponseEntity<Utilisateur> findAll() {
-		Utilisateur u = new Utilisateur(1L, "Maria", "maria@gmail.com", "99999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<Utilisateur>> findAll() {
+		List<Utilisateur> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Utilisateur> findById(@PathVariable Long id) {
+		Utilisateur obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
---
+++ src/main/java/com/example/aula301/services/UtilisateurService.java
@@ -0,0 +1,27 @@
+package com.example.aula301.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.aula301.entities.Utilisateur;
+import com.example.aula301.repositories.UtilisateurRepository;
+
+@Service
+public class UtilisateurService {
+	
+	@Autowired
+	private UtilisateurRepository repository;
+	
+	public List<Utilisateur> findAll() {
+		return repository.findAll();
+	}
+	
+	public Utilisateur findById(Long id) {
+		Optional<Utilisateur> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
